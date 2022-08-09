1adf3e36ae2e4898ca72ad43615dae90eeaaebc5
Shadient <85789237+Shadient@users.noreply.github.com>
2022-08-09 11:40:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,8 @@ source ~/.bashrc
   # Python3
   # Node & NPM
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-  
+  source ~/.bashrc
+  nvm install --lts
+  source ~/.bashrc
+  npm install -g npm@8.13.2
   # Rust
