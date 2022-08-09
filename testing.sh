d6a1782a87d3cbf6c746bc2e9398fe0f9c7737a6
Shadient <85789237+Shadient@users.noreply.github.com>
2022-08-09 12:56:44 +0200

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -22,19 +22,3 @@ source ~/.bashrc
   git clone https://github.com/alacritty/alacritty.git
   cd alacritty
   cargo build --release
-  ## terminfo
-  infocmp alacritty
-  sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
-  ## Desktop entry
-  sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
-  sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-  sudo desktop-file-install extra/linux/Alacritty.desktop
-  sudo update-desktop-database
-  ## Manual Page
-  sudo mkdir -p /usr/local/share/man/man1
-  gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
-  gzip -c extra/alacritty-msg.man | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
-  ## Shell Completion
-  mkdir -p ~/.bash_completion
-  cp extra/completions/alacritty.bash ~/.bash_completion/alacritty
-  echo "source ~/.bash_completion/alacritty" >> ~/.bashrc
