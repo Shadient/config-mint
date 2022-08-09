632fe46063ff871750477f648300ecf4ca99d361
Shadient <85789237+Shadient@users.noreply.github.com>
2022-08-09 11:40:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
+# build essential
+sudo apt install build-essential
+source ~/.bashrc
+
 # INSTALL LANGUAGES
-# Rust
+  # Python3
+  # Node & NPM
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  
+  # Rust
