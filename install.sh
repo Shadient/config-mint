d01188597c0d7130729e0fd3e6e176fd80e17893
Shadient <85789237+Shadient@users.noreply.github.com>
2022-08-09 13:18:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-# essentials
-sudo apt install build-essential gzip
+# ESSENTIALS
+sudo apt install build-essential git
 source ~/.bashrc
 
 # INSTALL LANGUAGES
@@ -11,7 +11,6 @@ source ~/.bashrc
   source ~/.bashrc
   nvm install --lts
   source ~/.bashrc
-  npm install -g npm@8.13.2
   
   # Rust
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -24,17 +23,17 @@ source ~/.bashrc
   cargo build --release
   ## terminfo
   infocmp alacritty
-  sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+  # sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
   ## Desktop entry
-  sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
-  sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-  sudo desktop-file-install extra/linux/Alacritty.desktop
-  sudo update-desktop-database
+  #sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+  #sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+  #sudo desktop-file-install extra/linux/Alacritty.desktop
+  #sudo update-desktop-database
   ## Manual Page
-  sudo mkdir -p /usr/local/share/man/man1
-  gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
-  gzip -c extra/alacritty-msg.man | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
+  #sudo mkdir -p /usr/local/share/man/man1
+  #gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+  #gzip -c extra/alacritty-msg.man | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
   ## Shell Completion
-  mkdir -p ~/.bash_completion
-  cp extra/completions/alacritty.bash ~/.bash_completion/alacritty
-  echo "source ~/.bash_completion/alacritty" >> ~/.bashrc
+  #mkdir -p ~/.bash_completion
+  #cp extra/completions/alacritty.bash ~/.bash_completion/alacritty
+  #echo "source ~/.bash_completion/alacritty" >> ~/.bashrc
